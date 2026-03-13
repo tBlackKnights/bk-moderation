c923b64d92d49f2b5a5db794d0b5a5f630516989
luarrekcah <raulrodriguesdemoraes@gmail.com>
2026-03-13 13:52:00 -0300

    feat: accepted by in accept message

--- src/bot/commands/moderation/accept.js
+++ src/bot/commands/moderation/accept.js
@@ -27,7 +27,7 @@ module.exports = {
 
             await interaction.editReply({ content: `✅ Successfully accepted the join request for **${username}** *(ID: ${userId})*.` });
 
-            const publicMessage = `**${username}** Accepted ✅, note: you're not officially in the guild, need join in game now.\n> Next Steps:\n- Join the guild in-game. Code: \`5717222\` ([PC Tutorial](https://discord.com/channels/685101896922431515/991428550441848842/1436421414314840126) / [Mobile Tutorial](SOON)]);\n- Send a fullscreen screenshot in https://discord.com/channels/685101896922431515/991428550441848842 to get your member roles.\n-# Welcome and please, always check the chat pins!`;
+            const publicMessage = `**${username}** Accepted ✅, note: you're not officially in the guild, need join in game now.\n> Next Steps:\n- Join the guild in-game. Code: \`5717222\` ([PC Tutorial](https://discord.com/channels/685101896922431515/991428550441848842/1436421414314840126) / [Mobile Tutorial](https://cdn.discordapp.com/attachments/995011257562304512/1482053850855772452/Screen_Recording_20260313_173241_Roblox.mp4?ex=69b58db5&is=69b43c35&hm=cd37dcf0b04304ecb23d97c846a3d9175340182e488c432e4368a75ceae68ac6&));\n- Send a fullscreen screenshot in https://discord.com/channels/685101896922431515/991428550441848842 to get your member roles.\n-# Welcome and please, always check the chat pins!\n-# Accepted by ${interaction.user.username} (${interaction.user.id})`;
             await interaction.channel.send({ content: publicMessage });
         } catch (error) {
             console.error("[Roblox API Error]", error.message);
